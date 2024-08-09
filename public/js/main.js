e36df6a3db89ac8541c319f9e4f150818441428f
Rasu2 <rasul2@gmail.com>
2024-08-09 22:26:17 +0500

    updated text

--- public/js/main.js
+++ public/js/main.js
@@ -87,8 +87,8 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show')
-        // } else {
-        //     entry.target.classList.remove('show')
+        } else {
+            entry.target.classList.remove('show')
         } 
     })
 })
